203808997804febb49cc06b77903143a7553f10b
Nagaraju9182 <nagraju.fresher@gmail.com>
2024-09-26 22:12:56 +0530

    added the new file

---
+++ index4.js
@@ -0,0 +1,23 @@
+let students = ["ameer","vamshi","gopal",10,20,30,40,50];
+
+let myStudents = students.slice(0,4)
+
+document.write(myStudents + "<br>")
+
+// __________________________________________________________
+
+
+let myNumbers = [0,1,2,3,4,5,6,7,8,9,]
+
+let myNewNumbers = myNumbers.splice(0,4, "something to you")
+
+document.write(myNumbers + "<br>");
+
+document.write(myNewNumbers + "<br> ")
+
+
+// ______________________________________________________________
+
+ var sample = "this is simple string text"
+   
+  document.write(sample.split(""))
